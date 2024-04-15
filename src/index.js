f3e3637fc24326f03c694f45ea08e999d6ba0b41
AnaliApps <ronoanali5@gmail.com>
2024-04-15 17:30:30 +0300

    Tab functionality
    I made changes such that when tab is clicked shows the right page on the main page.

--- src/index.js
+++ src/index.js
@@ -1,26 +1,25 @@
 import displayUI from "./display"
-import displayHomeUI from "./home"
 import displayMenuUI from "./menu";
 import displayAboutUI from "./about";
 import Icon from './icon.png';
-document.body.appendChild(displayUI(Icon))
+displayUI(Icon);
  let homebtn = document.querySelector("#home");
  let menubtn = document.querySelector("#menu");
  let aboutbtn = document.querySelector("#about");
  let content = document.querySelector("#content");
  console.log(homebtn)
  homebtn.addEventListener("click",(e)=>{
-    content.innerHTML = '';
-    content.appendChild(displayHomeUI("Home"));
+   content.innerHTML = '';
+    displayUI(Icon);
  })
 
  menubtn.addEventListener("click",()=>{
-    document.body.innerHTML = '';
-    document.body.appendChild(displayMenuUI("Menu"));
+    content.innerHTML = '';
+    content.appendChild(displayMenuUI("Menu"));
  })
 
  aboutbtn.addEventListener("click",()=>{
-    document.body.innerHTML = '';
-    document.body.appendChild(displayAboutUI("About"));
+    content.innerHTML = '';
+    content.appendChild(displayAboutUI("About"));
  })
 
