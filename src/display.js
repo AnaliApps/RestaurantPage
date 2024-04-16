c69dce68af2a6743ddece0b2733d2450ca186693
AnaliApps <ronoanali5@gmail.com>
2024-04-16 14:53:15 +0300

    Tab Contents
    I have added relevant contents of each tab and added some styling to make the site look presentable.

--- src/display.js
+++ src/display.js
@@ -2,11 +2,12 @@ import './style.css';
 export default function displayUI(img){
     const div = document.querySelector('#content');
     const h1 = document.createElement('h1');
-    h1.textContent = "Analizer Restaurant Home Page"
+    h1.textContent = "Analizer Restaurant"
+    h1.setAttribute("class","header")
     const Icon = new Image();
     Icon.src = img;
     const para = document.createElement('p');
-    para.textContent = "Welcome to Analizer Restaurant, where culinary excellence meets comfort dining. Indulge in a symphony of flavors crafted by our passionate chefs, showcasing a diverse menu that caters to every palate. From sizzling steaks to fresh seafood, aromatic curries to hearty pastas, we pride ourselves on delivering mouthwatering dishes that tantalize the taste buds. Each bite is a journey through exquisite ingredients and meticulous preparation, ensuring a dining experience that lingers long after the last forkful. Join us and elevate your dining adventure to new heights at [Your Restaurant Name]. Taste the difference, savor the moment, and make every meal memorable."
+    para.innerHTML = "Welcome to Analizer Restaurant, where culinary excellence meets comfort dining. Indulge in a symphony of flavors crafted by our passionate chefs, showcasing a diverse menu that caters to every palate. From sizzling steaks to fresh seafood, aromatic curries to hearty pastas, we pride ourselves on delivering mouthwatering dishes that tantalize the taste buds. Each bite is a journey through exquisite ingredients and meticulous preparation, ensuring a dining experience that lingers long after the last forkful. Join us and elevate your dining adventure to new heights at <strong>Analizer Restaurant</strong>. Taste the difference, savor the moment, and make every meal memorable."
     // div.appendChild(header)
     div.appendChild(h1);
     div.appendChild(Icon);
--- src/menu.js
+++ src/menu.js
@@ -1,10 +1,23 @@
 export default function displayMenuUI(){
+    let menuList = [{title:'CANARY',price:'$7.46',description:'Fresh slices of banana with Nutella.'},{title:'FRESA',price:'$9.33',description:'Fresh cut strawberries with Nutella.'},{title:'STRAW BANANA',price:'$9.33',description:'Fresh cut strawberries and banana with Nutella.'},{title:'SMORE',price:'$9.33',description:'Nutella and marshmallow with graham cracker topping and ABC sauce.'},{title:'LAST WEDNESDAY',price:'$10.46',description:'Egg,bacon,swiss cheese,mushroom,dijon mustard and spinach.'},{title:'ABCT',price:'$8.99',description:'Avocado,bacon,cheddar,tomato.'},{title:"CHICKEN BACON RANCH",price:'$10.46',description:'Chopped chicken,bacon,sharp cheddar, tomato,spinach ranch.'}]
     let element = document.createElement("div");
-    let h1 = document.createElement("h1");
-    h1.textContent=`Menu Tab`;
-    let para = document.createElement("p");
-    para.textContent = "Iam the best programmer alive today."
-    element.appendChild(h1);
-    element.appendChild(para);
+    menuList.forEach((item)=>{
+        let card = document.createElement("div");
+        card.setAttribute("class","card_items")
+        let h1 = document.createElement("h2");
+        h1.textContent=item.title;
+        let span = document.createElement("span");
+        span.textContent = item.price
+        let innerDiv = document.createElement("div");
+        innerDiv.setAttribute("class","card_desc")
+        let para = document.createElement("p");
+        para.textContent = item.description
+        innerDiv.appendChild(h1);
+        innerDiv.appendChild(span);
+        card.appendChild(innerDiv)
+        card.appendChild(para);
+        element.appendChild(card);
+    })
+    
     return element;
 }
